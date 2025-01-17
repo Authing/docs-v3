18e80fc9933db60e7017dc4043115d2c6ecdc650
lixueping <lixueping@authing.cn>
2025-01-17 11:01:28 +0800

    fix: support private deploy

--- docs/.vuepress/env.js
+++ docs/.vuepress/env.js
@@ -1,18 +1,16 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 // 加载根目录的config.yaml
 const { cosmiconfigSync } = require('cosmiconfig');
 // 确保config.yaml存在
-fs.appendFileSync(path.resolve(__dirname, '../../config.yaml'), '')
+fs.appendFileSync(path.resolve(__dirname, '../../config.yaml'), '');
 
 const config = cosmiconfigSync('config', {
-	searchPlaces: [
-		'config.yaml',
-	]
-}).search()
-
+  searchPlaces: ['config.yaml']
+}).search();
+console.log(process.env, '>>>>>>>>>>>>>>');
 module.exports = {
-	/** 站点基础路径 */
-	basePath: '/v3/',
-	config: config && config.config
+  /** 站点基础路径 */
+  basePath: process.env.SITE_BASE_PATH || '/v3/',
+  config: config && config.config
 };
